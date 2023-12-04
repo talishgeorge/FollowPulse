22ca90a154b4397a8f180c3e612b9223a2f5e2d5
Talish George <talishtest@gmail.com>
2023-12-05 10:05:00 +1100

    Fixes for bugs related to the size of collection view cells.

--- FollowPlus/FollowPlus/CustomViews/Cells/FollowerCell.swift
+++ FollowPlus/FollowPlus/CustomViews/Cells/FollowerCell.swift
@@ -9,51 +9,54 @@ import UIKit
 
 class FollowerCell: UICollectionViewCell {
     static let reuseID = "FollowerCell"
-    
+
     let avatraImageView = FPAvatraImageIvew(frame: .zero)
     let userNameLabel = FPTitleLabel(textAlignment: .center, fontSize: 16)
     var avatarImageViewHeightConstraint: NSLayoutConstraint?
-    
+
     override init(frame: CGRect) {
         super.init(frame: frame)
         configure()
         avatarImageViewHeightConstraint = avatraImageView.heightAnchor.constraint(equalTo: avatraImageView.widthAnchor)
-            avatarImageViewHeightConstraint?.isActive = true
+        avatarImageViewHeightConstraint?.isActive = true
+        // contentView.backgroundColor = .lightGray // Set the background color here
+        avatraImageView.backgroundColor = .lightGray // Set a contrasting color for the image view
+        userNameLabel.backgroundColor = .lightText // Set a contrasting color for the label
     }
-    
+
     override func layoutSubviews() {
         super.layoutSubviews()
         let itemWidth = contentView.frame.size.width
         avatarImageViewHeightConstraint?.constant = itemWidth
     }
-    
+
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not implemented")
     }
-    
+
     func set(follower: Follower) {
         userNameLabel.text = follower.login
     }
-    
+
+    /// <#Description#>
     private func configure() {
-        addSubview(avatraImageView)
-        addSubview(userNameLabel)
+        contentView.addSubview(avatraImageView)
+        contentView.addSubview(userNameLabel)
         let padding: CGFloat = 0
         avatraImageView.translatesAutoresizingMaskIntoConstraints = false
         userNameLabel.translatesAutoresizingMaskIntoConstraints = false
         userNameLabel.numberOfLines = 1 // Allow label to grow vertically
-        
+
         NSLayoutConstraint.activate([
-               avatraImageView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: padding),
-               avatraImageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: padding),
-               avatraImageView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -padding),
-               avatraImageView.widthAnchor.constraint(equalToConstant: 100), // Set a constant width
-               avatraImageView.heightAnchor.constraint(equalToConstant: 100),
-               userNameLabel.topAnchor.constraint(equalTo: avatraImageView.bottomAnchor, constant: 12),
-               userNameLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: padding),
-               userNameLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -padding),
-               userNameLabel.heightAnchor.constraint(equalToConstant: 20), // Set a constant height
-               userNameLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -padding) // Ensure label doesn't go beyond cell bounds
-           ])
+            avatraImageView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: padding),
+            avatraImageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: padding),
+            avatraImageView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -padding),
+            avatraImageView.heightAnchor.constraint(equalTo: avatraImageView.widthAnchor), // Make height equal to width
+
+            userNameLabel.topAnchor.constraint(equalTo: avatraImageView.bottomAnchor, constant: 12),
+            userNameLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: padding),
+            userNameLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -padding),
+            userNameLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -padding),
+        ])
     }
 }
--- FollowPlus/FollowPlus/FollowersListVC.swift
+++ FollowPlus/FollowPlus/FollowersListVC.swift
@@ -1,3 +1,4 @@
+
 //
 //  FollowersListVC.swift
 //  FollowPlus
@@ -7,95 +8,89 @@
 
 import UIKit
 
-class FollowersListVC: UIViewController /*UICollectionViewDelegateFlowLayout*/ {
+class FollowersListVC: UIViewController {
+    
     enum Section {
-           case main
-       }
-
-       var userName: String = "Talish"
-       var followers: [Follower] = []
-       var collectionView: UICollectionView!
-       var dataSource: UICollectionViewDiffableDataSource<Section, Follower>?
-
-       override func viewDidLoad() {
-           super.viewDidLoad()
-           view.backgroundColor = .systemBackground
-           configureViewController()
-           configureCollectionView()
-           configureDataSource()
-           getFollowers()
-       }
-
-       override func viewWillAppear(_ animated: Bool) {
-           super.viewWillAppear(animated)
-           navigationController?.setNavigationBarHidden(false, animated: true)
-       }
-
-       func configureViewController() {
-           view.backgroundColor = .systemBackground
-           navigationController?.isNavigationBarHidden = false
-           navigationController?.navigationBar.prefersLargeTitles = true
-       }
-
-       func configureCollectionView() {
-           collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: createThreeColumnFlowLayout(in: view))
-//           collectionView.delegate = self
-           view.addSubview(collectionView)
-           collectionView.backgroundColor = .systemBackground
-           collectionView.register(FollowerCell.self, forCellWithReuseIdentifier: FollowerCell.reuseID)
-       }
-
-       func createThreeColumnFlowLayout(in view: UIView) -> UICollectionViewFlowLayout {
-           let width = view.bounds.width
-           let padding: CGFloat = 12
-           let minimumItemSpacing: CGFloat = 10
-           let availableWidth = width - (padding * 2) - (minimumItemSpacing * 2)
-           let itemWidth = availableWidth / 3
-           let flowLayout = UICollectionViewFlowLayout()
-           flowLayout.sectionInset = UIEdgeInsets(top: padding, left: padding, bottom: padding, right: padding)
-           flowLayout.itemSize = CGSize(width: itemWidth, height: itemWidth + 40)
-           return flowLayout
-       }
-
-       func getFollowers() {
-           NetworkManager.shared.getFollower(for: userName, page: 1) { result in
-               switch result {
-               case let .success(followers):
-                   self.followers = followers
-                   self.updateData()
-               case let .failure(error):
-                   print(error)
-               }
-           }
-       }
-
-       func configureDataSource() {
-           dataSource = UICollectionViewDiffableDataSource<Section, Follower>(collectionView: collectionView, cellProvider: { collectionView, indexPath, follower -> UICollectionViewCell? in
-               let cell = collectionView.dequeueReusableCell(withReuseIdentifier: FollowerCell.reuseID, for: indexPath) as! FollowerCell
-               cell.set(follower: follower)
-               return cell
-           })
-       }
-
-       func updateData() {
-           var snapshot = NSDiffableDataSourceSnapshot<Section, Follower>()
-           snapshot.appendSections([.main])
-           snapshot.appendItems(followers)
-           DispatchQueue.main.async {
-               self.dataSource?.apply(snapshot, animatingDifferences: true)
-           }
-       }
-
-//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-//        guard let flowLayout = collectionViewLayout as? UICollectionViewFlowLayout else {
-//            return CGSize(width: 100, height: 100)
-//        }
-//        let insets = flowLayout.sectionInset.left + flowLayout.sectionInset.right
-//        let spacing = flowLayout.minimumInteritemSpacing * 2
-//        let availableWidth = collectionView.frame.width - insets - spacing
-//        let itemWidth = availableWidth / 3
-//        return CGSize(width: itemWidth, height: itemWidth + 40)
-//    }
+        case main
+    }
+    
+    var userName: String = "Talish"
+    var followers: [Follower] = []
+    var collectionView: UICollectionView!
+    var dataSource: UICollectionViewDiffableDataSource<Section, Follower>?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        configureViewController()
+        configureCollectionView()
+        configureDataSource()
+        getFollowers()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.setNavigationBarHidden(false, animated: true)
+    }
+    
+    func configureViewController() {
+        view.backgroundColor = .systemBackground
+        navigationController?.isNavigationBarHidden = false
+        navigationController?.navigationBar.prefersLargeTitles = true
+    }
+    
+    func configureCollectionView() {
+        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: createThreeColumnFlowLayout(in: view))
+        view.addSubview(collectionView)
+        collectionView.backgroundColor = .systemBackground
+        collectionView.register(FollowerCell.self, forCellWithReuseIdentifier: FollowerCell.reuseID)
+    }
+    
+    func createThreeColumnFlowLayout(in view: UIView) -> UICollectionViewFlowLayout {
+        let width = view.bounds.width
+        let padding: CGFloat = 12
+        let minimumItemSpacing: CGFloat = 10
+        let availableWidth = width - (padding * 2) - (minimumItemSpacing * 2)
+        let itemWidth = availableWidth / 3
+        let itemHeight = itemWidth + 40 // Height is width + 40
+        let flowLayout = UICollectionViewFlowLayout()
+        flowLayout.sectionInset = UIEdgeInsets(top: padding, left: padding, bottom: padding, right: padding)
+        flowLayout.itemSize = CGSize(width: itemWidth, height: itemHeight)
+        return flowLayout
+    }
+    
+    func getFollowers() {
+        NetworkManager.shared.getFollower(for: userName, page: 1) { result in
+            switch result {
+            case let .success(followers):
+                self.followers = followers
+                self.updateData()
+            case let .failure(error):
+                print(error)
+            }
+        }
+    }
+    
+    func configureDataSource() {
+        dataSource = UICollectionViewDiffableDataSource<Section, Follower>(collectionView: collectionView, cellProvider: { collectionView, indexPath, follower -> UICollectionViewCell? in
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: FollowerCell.reuseID, for: indexPath) as! FollowerCell
+            cell.set(follower: follower)
+            return cell
+        })
+    }
+    
+    func updateData() {
+        var snapshot = NSDiffableDataSourceSnapshot<Section, Follower>()
+        snapshot.appendSections([.main])
+        snapshot.appendItems(followers)
+        DispatchQueue.main.async {
+            self.dataSource?.apply(snapshot, animatingDifferences: true)
+        }
+    }
+   
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: collectionView.frame.width, height: 150) // Set a constant height
+    }
 }
 
 #Preview("FollowersListVC") {
